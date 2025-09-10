1914747c2b1a2a7346c48e157b4a27a52fb9897f
swanujoshi <joshiswanand20@gmail.com>
2025-09-10 22:12:25 +0530

    Creation of react and vite project and rendering one compnent

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h2>Chai and React</h2>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+import Chai from "./Chai"
+function App() {
+  return (
+    <>
+    <Chai />
+     <h1>React and swananad</h1>
+    </>
+  )
+}
+export default App
